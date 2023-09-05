c841e784312cfbae8b003f54402cd25189caee21
Kevin Dierkx <kevin@distortedfusion.com>
2023-09-05 14:22:57 +0200

    Moved vendor views and updated namespace

--- config/blade-forms.php
+++ config/blade-forms.php
@@ -1,6 +1,6 @@
 <?php
 
-use DistortedFusion\Tailwind\Components;
+use DistortedFusion\BladeForms\Components;
 
 return [
     'components' => [
@@ -35,34 +35,34 @@ return [
 
     'form-components' => [
         'form' => [
-            'view' => 'blade-forms::components.vendor.form-components.form',
+            'view' => 'blade-forms::vendor.form-components.form',
         ],
         'form-checkbox' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-checkbox',
+            'view' => 'blade-forms::vendor.form-components.form-checkbox',
         ],
         'form-errors' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-errors',
+            'view' => 'blade-forms::vendor.form-components.form-errors',
         ],
         'form-group' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-group',
+            'view' => 'blade-forms::vendor.form-components.form-group',
         ],
         'form-input' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-input',
+            'view' => 'blade-forms::vendor.form-components.form-input',
         ],
         'form-label' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-label',
+            'view' => 'blade-forms::vendor.form-components.form-label',
         ],
         'form-radio' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-radio',
+            'view' => 'blade-forms::vendor.form-components.form-radio',
         ],
         'form-select' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-select',
+            'view' => 'blade-forms::vendor.form-components.form-select',
         ],
         'form-submit' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-submit',
+            'view' => 'blade-forms::vendor.form-components.form-submit',
         ],
         'form-textarea' => [
-            'view' => 'blade-forms::components.vendor.form-components.form-textarea',
+            'view' => 'blade-forms::vendor.form-components.form-textarea',
         ],
     ],
 ];
