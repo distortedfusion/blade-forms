03daa72ad8d1363857e7c886c13e3d351c9f9f74
Kevin Dierkx <kevin@distortedfusion.com>
2024-05-07 00:36:45 +0200

    Disabled support for radio/checkbox buttons

--- resources/views/components/checkbox-button.blade.php
+++ resources/views/components/checkbox-button.blade.php
@@ -3,6 +3,7 @@
         'w-full flex flex-col',
         'bg-brand-500/0 border border-black/20 ring-1 ring-brand-500/0 rounded-md',
         '[&:has(input:checked)]:bg-brand-500/10 [&:has(input:checked)]:border-brand-500 [&:has(input:checked)]:ring-brand-500',
+        '[&:has(input:disabled)]:bg-gray-100 [&:has(input:disabled)]:border-gray-300',
         'transition-[background-color,_box-shadow,_border-color]',
     ])}}">
         <label class="w-full flex items-center px-4 py-2 cursor-pointer">
