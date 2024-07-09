763c2625faaf35173f377b0276c0f7f2785c66d1
Kevin Dierkx <kevin@distortedfusion.com>
2024-07-09 19:19:19 +0200

    Cleanup

--- config/blade-forms.php
+++ config/blade-forms.php
@@ -4,29 +4,23 @@ use DistortedFusion\BladeForms\Components;
 
 return [
     'components' => [
-        'form-grid' => 'blade-forms::components.grid.index',
-        'form-grid-column' => 'blade-forms::components.grid.column',
-
-        'form-section' => Components\Section::class,
-        'form-section-title' => Components\SectionTitle::class,
-        'form-sections' => 'blade-forms::components.section-group',
-
-        // Form Elements...
-        'form-help' => Components\Help::class,
-        'form-icon' => 'blade-forms::components.icon',
-        'form-pulser' => 'blade-forms::components.pulser',
-        'form-select-description' => Components\SelectDescription::class,
-        'form-toggle' => Components\Toggle::class,
-
-        // Form components...
-        'form' => Components\Form::class,
         'form-checkbox' => Components\Checkbox::class,
         'form-errors' => Components\Errors::class,
+        'form-grid-column' => 'blade-forms::components.grid.column',
+        'form-grid' => 'blade-forms::components.grid.index',
         'form-group' => Components\Group::class,
+        'form-help' => Components\Help::class,
+        'form-icon' => 'blade-forms::components.icon',
         'form-input' => Components\Input::class,
         'form-label' => Components\Label::class,
         'form-radio' => Components\Radio::class,
+        'form-section-title' => Components\SectionTitle::class,
+        'form-section' => Components\Section::class,
+        'form-sections' => 'blade-forms::components.section-group',
+        'form-select-description' => Components\SelectDescription::class,
         'form-select' => Components\Select::class,
         'form-textarea' => Components\Textarea::class,
+        'form-toggle' => Components\Toggle::class,
+        'form' => Components\Form::class,
     ],
 ];
