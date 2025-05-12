f08a1b1795159b327b3d40a29a79d98bc5428d78
Kevin Dierkx <kevin@distortedfusion.com>
2025-05-12 14:42:06 +0200

    Rotated spacing

--- resources/views/components/section-group.blade.php
+++ resources/views/components/section-group.blade.php
@@ -1,6 +1,6 @@
 <div {{ $attributes->class([
-    'flex flex-col gap-y-4 divide-y divide-[var(--border)]',
-    '[&>*]:pt-4 [&>*:first-child]:pt-0',
+    'flex flex-col gap-y-4 [&_section:not(:last-child)]:pb-4',
+    'divide-y divide-[var(--border)]',
 ]) }}>
     {{ $slot }}
 </div>
