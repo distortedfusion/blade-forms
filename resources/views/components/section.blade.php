b217229d124fb2681ad66d5d814951ba60a2be03
Kevin Dierkx <kevin@distortedfusion.com>
2025-11-17 11:53:37 +0100

    Updated section element to use alias

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,8 +1,13 @@
+@php
+use DistortedFusion\BladeForms\BladeForms;
+@endphp
 <section {{ $attributes->class(['flex flex-col gap-y-4']) }}>
     @if($title)
         <div class="flex items-center justify-between gap-x-2">
             <div class="flex-grow">
-                <x-form-section-title>{{ $title }}</x-form-section-title>
+                <x-dynamic-component :component="BladeForms::componentAliasWithPrefix('form-section-title')">
+                    {{ $title }}
+                </x-dynamic-component>
                 @if($description)
                     <p class="text-[var(--muted-foreground)] text-sm">{{ $description }}</p>
                 @endif
@@ -14,18 +19,20 @@
         </div>
     @endif
 
-    <x-form-grid :attributes="$getGridAttributeBag()->class([
-        'gap-x-4 gap-y-2',
+    <x-dynamic-component
+        :component="BladeForms::componentAliasWithPrefix('form-grid')"
+        :attributes="$getGridAttributeBag()->class([
+            'gap-x-4 gap-y-2',
 
-        'items-start' => $alignItems === 'start',
-        'items-end' => $alignItems === 'end',
-        'items-end-safe' => $alignItems === 'end-safe',
-        'items-center' => $alignItems === 'center',
-        'items-center-safe' => $alignItems === 'center-safe',
-        'items-baseline' => $alignItems === 'baseline',
-        'items-baseline-last' => $alignItems === 'baseline-last',
-        'items-stretch' => $alignItems === 'stretch',
-    ])">
+            'items-start' => $alignItems === 'start',
+            'items-end' => $alignItems === 'end',
+            'items-end-safe' => $alignItems === 'end-safe',
+            'items-center' => $alignItems === 'center',
+            'items-center-safe' => $alignItems === 'center-safe',
+            'items-baseline' => $alignItems === 'baseline',
+            'items-baseline-last' => $alignItems === 'baseline-last',
+            'items-stretch' => $alignItems === 'stretch',
+        ])">
         {{ $slot }}
-    </x-form-grid>
+    </x-dynamic-component>
 </section>
